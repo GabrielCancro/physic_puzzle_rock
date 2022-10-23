c81bfdff0e1abcf633d60ae4edd73226badb301c
Gabriel Cancro <gabrielcancro@3ogs.com.ar>
2022-10-23 11:58:52 -0300

    update

--- Camera2D.gd
+++ Camera2D.gd
@@ -5,11 +5,13 @@ onready var BALL = get_node("/root/Game/Ball")
 
 func _process(delta):
 	if Input.is_action_pressed("ui_left"):
-		rotation_degrees -= rotate_vel
-	if Input.is_action_pressed("ui_right"):
 		rotation_degrees += rotate_vel
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("ui_right"):
+		rotation_degrees -= rotate_vel
+	if Input.is_action_pressed("ui_accept"):
 		BALL.propulse()
+#	if Input.is_action_pressed("ui_up"):
+#		BALL.jump()
 	
 	position = BALL.position
 	BALL.dir = Vector2( cos(rotation+PI/2), sin(rotation+PI/2) )
--- UI.gd
+++ UI.gd
@@ -1,12 +1,38 @@
 extends CanvasLayer
 
 var lives = 3
+var segs = 120
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Timer.connect("timeout",self,"onTimeTick")
+	onTimeTick(0)
+	yield(get_tree().create_timer(.1),"timeout")
+	$START.visible = true
+	get_tree().paused = true
+	for i in $START.get_children(): i.modulate.a = 0
+	yield(get_tree().create_timer(1),"timeout")
+	for i in $START.get_children(): 
+		$Tween.interpolate_property(i,"modulate",Color(1,1,1,0),Color(1,1,1,1),.5,Tween.TRANS_QUAD,Tween.EASE_OUT,i.get_index()*1)
+	$Tween.start()
 
+func onTimeTick(ticks = 1):
+	segs -= ticks
+	var m = floor(segs/60)
+	$lb_time.text = "%d:%02d" % [m, segs-m*60]
+	if(segs == 0):
+		lives = 0
+		hit_lives()
+
+func _process(delta):
+	if $START.visible && $START/Label4.modulate.a == 1 && Input.is_action_just_pressed("ui_accept"):
+		$Tween.interpolate_property($START,"modulate",Color(1,1,1,1),Color(1,1,1,0),1,Tween.TRANS_QUAD,Tween.EASE_IN)
+		$Tween.start()
+		yield(get_tree().create_timer(1.5),"timeout")
+		$START.visible = false
+		get_tree().paused = false
+		$Timer.start()
+	if Input.is_action_just_pressed("ui_end"): get_tree().quit(0)
+	
 func hit_lives():
 	lives -= 1
 	if lives<3: $life3.modulate = Color(.1,.1,.1)
@@ -15,5 +41,6 @@ func hit_lives():
 	if lives<0: 
 		get_tree().paused = true
 		$LOST.visible = true
+		$Timer.stop()
 		yield(get_tree().create_timer(2.5),"timeout")
-		get_tree().quit(0)
+		get_tree().quit(2)
